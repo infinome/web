86627640e36340054ce2864f54c137dd1f7bcc11
DHeld000 <dave.held@comcast.com>
2020-10-23 22:50:16 -0700

    Updates

---
+++ precache-manifest.6012ccd4d0319bff564b91950800e2be.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94ae14ce768c0821c556601b30ee576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f37aa21a9ca2c93bc849",
+    "url": "/static/js/2.6b57b692.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6b57b692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba72eaafcbb0bb0e511",
+    "url": "/static/js/main.3245939f.chunk.js"
+  },
+  {
+    "revision": "29df65af98b369074e03",
+    "url": "/static/js/runtime-main.58c8fb25.js"
+  },
+  {
+    "revision": "a837d23c1d2548e60d2979613b68ec8d",
+    "url": "/static/media/Team-Andrew.a837d23c.png"
+  },
+  {
+    "revision": "09ecb362e1ee783415112b08be99700b",
+    "url": "/static/media/Team-Dan.09ecb362.png"
+  },
+  {
+    "revision": "a618560e31819bef5c774d6a339400e4",
+    "url": "/static/media/Team-Mike.a618560e.png"
+  },
+  {
+    "revision": "ed6aab2aeb63a058abb8493d392246c1",
+    "url": "/static/media/Team-Richard.ed6aab2a.png"
+  },
+  {
+    "revision": "5f2d4c000198d66e664b47e6a5e4851a",
+    "url": "/static/media/boulder-mountains.5f2d4c00.png"
+  },
+  {
+    "revision": "facf82bbe9853399e7e0b485cf3159b9",
+    "url": "/static/media/thumbnail-SynBioBeta-Inscripta-Webinar-Precision.facf82bb.png"
+  },
+  {
+    "revision": "2134a70917440f63b018f138b3b3083e",
+    "url": "/static/media/thumbnail-a-benchtop-platform-for-massively.2134a709.png"
+  },
+  {
+    "revision": "ed9d743c8c0a77d92426bcdaa3668aee",
+    "url": "/static/media/thumbnail-genome-scale-mapping-of-genotype.ed9d743c.png"
+  },
+  {
+    "revision": "04d3dfbf343bac0c471bd4eea66804c2",
+    "url": "/static/media/thumbnail-rapid-forward-engineering.04d3dfbf.png"
+  },
+  {
+    "revision": "5bbf1f65465586a728216429f4fa7263",
+    "url": "/static/media/thumbnail-rapid-genome-scale-engineering.5bbf1f65.png"
+  }
+]);
